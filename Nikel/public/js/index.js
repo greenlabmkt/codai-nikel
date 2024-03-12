caa0685ced31b8cdd41c8519e818a9d5692027ad
greenlabmkt <163185533+greenlabmkt@users.noreply.github.com>
2024-03-12 15:51:20 -0300

    Add files via upload

---
+++ Nikel/public/js/index.js
@@ -0,0 +1,9 @@
+
+//CRIAR CONTA
+document.getElementById("create-acc").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    alert("Conta criada.");
+});
+
+//esse codigo nao rodou, nao consegui sair deste ponto
